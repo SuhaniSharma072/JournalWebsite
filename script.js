25c53e1a4f0eefa196b67884115c802b351a47fd
SuhaniSharma072 <suhanisharma072@gmail.com>
2025-10-29 21:13:19 -0500

    initialize script.js with DOM element selections for popup functionality

---
+++ script.js
@@ -0,0 +1,6 @@
+const addBox=document.querySelector(".add-box"),
+popupBox=document.querySelector(".popup-box"),
+closeIcon=popupBox.querySelector("header i"),
+titleTag=popupBox.querySelector("input"),
+descTag=popupBox.querySelector("header i"),
+addBtn=popupBox.querySelector("button");
